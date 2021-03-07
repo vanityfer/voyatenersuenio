6cae3a63b58751af0ab443a22e02b8f58c0880c7
vanityfer <75648982+vanityfer@users.noreply.github.com>
2021-03-06 21:59:04 -0300

    Update verse.js

--- verse.js
+++ verse.js
@@ -32,7 +32,6 @@ let VERSES = {
     "cruzábamos de la mano",
      "",
     "soñé con un lugar mágico al que se podía entrar desde mi casa",
-     "",
     "soñé que juan ariel gomez me decía sos un embole en blanco y negro",
     "dormí siete minutos y soñé",
     "que bajaba once pisos por una escalera hecha con sogas",
@@ -56,7 +55,6 @@ let VERSES = {
      "",
      "soñé que ponía chubby checker para bailar",
     "alguien lo cambiaba y ponía una canción triste de calamaro y se reía de mí",
-     "",
     "soñé que era el 2066 ",
     "caminaba sola en un supermercado toledo",
       "",
@@ -64,8 +62,8 @@ let VERSES = {
     "para recuperar un paraguas blanco",
     "soñé que tomaba horas",
     "para trabajar en el baño de un boliche",
+    "soñé con un lobo marino re lindo", 
      "",
-     "soñé con un lobo marino re lindo", 
      "soñé con sartre pero no puedo recordar qué",
      "soñé que mario ortiz me decía", 
      "el año que viene se van a usar los aparatos sin dientes",
@@ -78,6 +76,14 @@ let VERSES = {
      "soñé", 
      "que nos atacaban policías desde autos voladores", 
      "", 
+     "soñé que godard había filmado una película en mar del plata en 1950", 
+     "soñé que un perro me atacaba", 
+     "soñé que iba a un cumpleaños y estaban todos en silencio", 
+     "soñé que había una fiesta", 
+     "en lo de mis vecinos jorge y laura", 
+     "también soñé con pokèmon", 
+     "", 
+     
      
   ]
    
