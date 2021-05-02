562d5fab11ca260e73a1114cbf8082ca637dedf9
vanityfer <75648982+vanityfer@users.noreply.github.com>
2021-05-02 13:15:13 -0300

    Update verse.js

--- verse.js
+++ verse.js
@@ -2,7 +2,7 @@
 let NAMES = [
    
   "soñé",
-  "soñé",
+  
   ];
 
 let VERSES = {
@@ -84,6 +84,95 @@ let VERSES = {
      "en lo de mis vecinos jorge y laura", 
      "también soñé con pokèmon", 
      "", 
+     "soñé q era el fin del mundo, había colectivos a los nuevos planetas, subía Néstor Kirchner y no tenía cargada la sube. nosotros le pagábamos el boleto",
+     "ayer soñé que me sacaba un 2 (dos)",
+     "soñé con La pérdida del reino de Bianco, sentía nostalgia de la época en que la leía",
+     "soñé que le daba una bebé a los lobos", 
+     "",
+     "tengo lo que soñé en la punta de la lengua",
+     "",
+     "soñé que el árbol de la puerta de casa era un limonero",
+     "soñé que si cambiaba una letra de mi nombre me transformaba en otra persona y vos no me querías",
+     "aww soñé con gatitos diminutos ahora que recuerdo",
+     "soñé que tenía una pantera que era buena solamente conmigo",
+     "",
+     "soñé con él", 
+     "", 
+     "soñé que Borges era literatura sin TACC", 
+     "soñé con mariposas gigantes que me hablaban",
+     "soñé que estaba empecinada", 
+     "soñé un poema de Vallejo", 
+     "soñé q era lunes y me había olvidado de ir a trabajar", 
+     "", 
+     "soñé que nos perseguía el nazismo", 
+     "soñé con Marruecos", 
+     "soñé con uno", 
+     "soñé que daba clases de cocinar en el aula del colegio con una hornalla carcelaria y aceite hirviendo", 
+     "soñé que tenía alzheimer", 
+     "", 
+     "soñé que volaba una sombrilla en la playa y se me clavaba en el dedo gordo del pie", 
+     "soñé que estaba en la orilla y el mar me llevaba", 
+     "soñé que me hacía amiga de un cura remisero. me llevaba a bailar a Chocolate"
+     "", 
+     "soñé que el hoy era un lugar a donde venían a parar todos los perros perdidos", 
+     "", 
+     "soñé que encontraba un libro de poemas de Roque Vaca Narvaja", 
+     "soñé que Valentín me decía 'hola’", 
+     "soñé que me echaban y me sangró la nariz durante la noche", 
+     "soñé que Manuel Gálvez cantaba una canción de Lana del Rey", 
+     "en la siesta soñé que tenía que cuidar a una perrita llamada Mulva", 
+     "soñé que buceaba en un bosque tipo Twin Peaks y me daba miedo", 
+     "soñé que María levantaba vuelo con un paracaídas y volaba re feliz", 
+     "", 
+     "soñé que elegía mal", 
+     "", 
+     "soñé que vendían droga en El Cartucho Feliz", 
+     "soñé que abrazaba una nube en un edificio muy alto", 
+     "no sé si soñé que me llegaba un mail o de verdad llegó", 
+     "soñé que le decía a alguien suerte, éxitos, lo que haga falta", 
+     "",
+     "después soñé algo que me mató para siempre", 
+     "", 
+     "soñé que había una combinación de movimientos del cuerpo con la que te morías", 
+     "soñé que un montón de zombies judíos venían a mar del plata", 
+     "soñé que construíamos una casa en el mar, abajo del agua. echaba raíces y teníamos que declararla en afip", 
+     "soñé con mi mamá y con otra persona llamada igual que ella", 
+     "", 
+     "soñé una temporada nueva y entera de Breaking Bad. Diego era walter white", 
+     "soñé que buscaba en google y los resultados no eran personalizados. era muy ominoso", 
+     "soñé que elegíamos lugares a donde ir en un mapa lleno de cadenas montañosas", 
+     "soñé que un bebé me decía 'me quiero morir’", 
+     "",
+     "soñé con mi ex psicólogo", 
+     "", 
+     "soñé que no podía hablar y me sangraba mucho la nariz", 
+     "soñé que me encontraba con mamá", 
+     "soñé que estaba borracha", 
+     "soñé que me hundía", 
+     "soñé que nadie me quería y me enojaba", 
+     "soñé con la ciénaga", 
+     "", 
+     "también soñé que llevaba a mis alumnos a un paseo. se perdían todos y los tenía que pagar", 
+     "soñé que me robaban la médula", 
+     "soñé que le preguntaba a mamá de dónde había sacado cada una de sus plantas", 
+     "soñé con el curso de lingüística general, con lo que dice el curso de lingüística general", 
+     "soñé que con María Rosario teníamos que ir a Chapadmalal y nos tomábamos un colectivo a Polonia", 
+     "", 
+     
+
+
+
+
+     
+
+
+
+
+
+
+
+
+     
      
      
   ]
