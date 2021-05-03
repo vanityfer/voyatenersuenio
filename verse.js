a2390a841d63f81571921668a2cabd3961a89886
vanityfer <75648982+vanityfer@users.noreply.github.com>
2021-05-02 21:30:42 -0300

    Update verse.js

--- verse.js
+++ verse.js
@@ -1,7 +1,7 @@
 
 let NAMES = [
    
-  "    soñé",
+  " soñé",
   "  soñé", 
   "   soñé", 
   "    soñé", 
@@ -22,7 +22,7 @@ let NAMES = [
   ];
 
 let VERSES = {
-  "    soñé": [
+  " soñé": [
     "soñé que te daba una cachetada",
     "soñé que no",
     "me dormí un ratito y soñé",
